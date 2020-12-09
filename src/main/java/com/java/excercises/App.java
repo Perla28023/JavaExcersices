76210cf0c33c446762c8b485ba38db3a0ac7bc0a
Perla Torres <perlatorresp>
2020-12-08 21:49:05 -0600

    Agregué operaciones

--- src/main/java/com/java/excercises/App.java
+++ src/main/java/com/java/excercises/App.java
@@ -4,10 +4,36 @@ package com.java.excercises;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		String titulo="Operaciones Matemáticas";
+		System.out.println(titulo); 
+		
+		System.out.println();
+		System.out.println("Suma");
+		int numA = 5;
+		int numB = 2;
+		
+		float numC =10;
+		float numD =3;
+		
+		double numE =100;
+		double numF =27;
+		
+		
+		System.out.println("La suma de A y B es: " + (numA + numB));
+		System.out.println();
+
+		System.out.println("Resta");
+		System.out.println("La resta de A y B es: " + (numA - numB));
+		System.out.println();
+
+		System.out.println("División");
+		System.out.println("La división de E y F es: " + (numE / numF));
+		System.out.println();
+
+		System.out.println("Multiplicación");
+		System.out.println("La multiplicación de A y B es: " + (numA * numB));
+
+	}
 }
