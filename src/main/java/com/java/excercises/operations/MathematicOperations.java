13ab14799e6647369c4108e9239752413d26959a
Perla Torres <perlatorresp>
2020-12-09 21:16:07 -0600

    Multiply Operation

--- src/main/java/com/java/excercises/operations/MathematicOperations.java
+++ src/main/java/com/java/excercises/operations/MathematicOperations.java
@@ -7,11 +7,18 @@ public class MathematicOperations {
 		return result;
 	}
 
+	public double getMultiplyOperation(double numA, double numB) {
+		double result;
+		result = numA * numB;
+		return result;
+	}
+
 	public static void main(String[] args) {
 		MathematicOperations operation = new MathematicOperations();
 		double result;
-		result = operation.getPlusOperation(22.5, 14);
+		result = operation.getMultiplyOperation(22.7, 14);
 		System.out.println("Result:" + result);
 
 	}
+
 }
